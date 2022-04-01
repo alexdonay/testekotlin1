92e784f807245fbdfd1949d51bc32767327563e3
Alexsander <alexdonay@gmail.com>
2022-04-01 16:24:32 -0300

    Initial commit

---
+++ src/primeirocodigo.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>){
+    println("Hello world ${max(1,2)}")
+//loops
+    //do while
+    var a : Int = 0
+    do {
+        println("O indice é  $a")
+        a++
+    }while (a < 10)
+
+}
+
+
+
+fun max(a: Int, b: Int): Int{
+
+    var y: Int = 15
+    y= 13
+    return y * (a+b)
+
+}
+
+
